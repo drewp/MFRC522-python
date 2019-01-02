cf7f28f25c77bc5e94bfc7663c0400627fa8d4c4
Drew Perttula <drewp@bigasterisk.com>
2019-01-02 11:30:24 -0800

    0.0.2 actually use the spidev dependency instead of 'spi'

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='MFRC522',
-    version='0.0.1',
+    version='0.0.2',
     description='Raspberry Pi Python library for SPI RFID RC522 module.',
     install_requires=['spidev', 'RPi.GPIO'],
     packages=['MFRC522'],
